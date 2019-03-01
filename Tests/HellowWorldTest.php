df0232f772f76126158545bca0dafdd21e953428
cutehackers <cutehackers@gmail.com>
2019-03-01 22:36:33 +1300

    modify php version

--- Tests/HellowWorldTest.php
+++ Tests/HellowWorldTest.php
@@ -1,8 +1,6 @@
 <?php
 
-require_once ('PHPUnit/Framework/TestCase.php');
-
-class HelloWorldTest extends TestCase
+class HelloWorldTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
